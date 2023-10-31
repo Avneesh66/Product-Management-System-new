1ef52bd96b8adf59eb96edc99df6a7e63ad2d93b
Avaneesh Srivastava <avaneeshs@successive.com>
2023-10-31 13:14:43 +0530

    Initial Commit

---
+++ Product Management system new/Product Management system new/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Product_Management_system_new.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
